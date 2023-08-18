21d9d3de8f97a3ffaa80098259a0a145e75b62a8
alexrnguyen <alexander.nguyen@shaw.ca>
2023-08-18 17:22:41 -0600

    Refactor code using template design pattern

--- scraper.py
+++ scraper.py
@@ -10,24 +10,33 @@ def createWorkbook():
     return workbook
 
 
-def getNHLPageSpreadsheet(workbook):
+def getSpreadsheet(workbook, source):
     # Source: https://stackoverflow.com/questions/75771237/error-parsing-cert-retrieved-from-aia-as-der-error-couldnt-read-tbscertifi
     options = webdriver.ChromeOptions()
     options.add_experimental_option("excludeSwitches", ["enable-logging"])
-
-    # Source: https://www.selenium.dev/selenium/docs/api/py/index.html
-    nhl_page_url = "https://www.nhl.com/news/nhl-fantasy-hockey-top-250-200-rankings-drafts-players-big-board/c-281505474"
     browser = webdriver.Chrome(options=options)
-    browser.get(nhl_page_url)
 
-    # Find the highest ranked player
-    output = browser.find_element(By.XPATH, "//p[contains(text(), '1. ')]").text
+    if source == "NHL.com":
+        url = "https://www.nhl.com/news/nhl-fantasy-hockey-top-250-200-rankings-drafts-players-big-board/c-281505474"
+        browser.get(url)
+        # Find the highest ranked player
+        output = browser.find_element(By.XPATH, "//p[contains(text(), '1. ')]").text
+    else:
+        url = "https://www.espn.com/fantasy/hockey/story/_/id/37963929/top-250-fantasy-nhl-rankings-scorers-goalies-2023-24"
+        browser.get(url)
+        # Find the highest ranked player (in each half)
+        firstHalf = browser.find_element(By.XPATH, "//p[contains(text(), '1. ')]").text
+        secondHalf = browser.find_element(
+            By.XPATH, "//p[contains(text(), '151. ')]"
+        ).text
+        output = "{0}\n{1}".format(firstHalf, secondHalf)
+
     browser.quit()
 
-    worksheet = workbook.add_worksheet("NHL.com")
-
+    worksheet = workbook.add_worksheet(source)
     colNames = ["Rank", "Name", "Position", "Team"]
     worksheet.write_row(0, 0, colNames)
+
     # Create an array from the output
     rankings = output.split("\n")
 
@@ -37,56 +46,21 @@ def getNHLPageSpreadsheet(workbook):
         for char in player:
             if char in punctuation:
                 player = player.replace(char, "")
+
         playerInfo = player.split(" ")[:5]
+        playerInfo[0] = int(playerInfo[0])
         playerInfo[1] = "{0} {1}".format(playerInfo[1], playerInfo[2])
         playerInfo.pop(2)
-        worksheet.write_row(row, 0, playerInfo)
-        row += 1
 
+        if source == "ESPN":
+            playerInfo[-1] = playerInfo[-1].upper()  # Uppercase team abbreviations
 
-def getESPNSpreadsheet(workbook):
-    # Source: https://stackoverflow.com/questions/75771237/error-parsing-cert-retrieved-from-aia-as-der-error-couldnt-read-tbscertifi
-    options = webdriver.ChromeOptions()
-    options.add_experimental_option("excludeSwitches", ["enable-logging"])
-
-    # Source: https://www.selenium.dev/selenium/docs/api/py/index.html
-    espn_url = "https://www.espn.com/fantasy/hockey/story/_/id/37963929/top-250-fantasy-nhl-rankings-scorers-goalies-2023-24"
-    browser = webdriver.Chrome(options=options)
-    browser.get(espn_url)
-
-    # Find the highest ranked player (in each half)
-    firstHalf = browser.find_element(By.XPATH, "//p[contains(text(), '1. ')]").text
-    secondHalf = browser.find_element(By.XPATH, "//p[contains(text(), '151. ')]").text
-    output = "{0}\n{1}".format(firstHalf, secondHalf)
-    browser.quit()
-
-    worksheet = workbook.add_worksheet("ESPN")
-
-    colNames = ["Rank", "Name", "Position", "Team"]
-    worksheet.write_row(0, 0, colNames)
-
-    # Create an array from the output
-    rankings = output.split("\n")
-
-    # Insert player info into spreadsheet
-    row = 1
-    for player in rankings:
-        # Remove commas and periods
-        for char in player:
-            if char in punctuation:
-                player = player.replace(char, "")
-        playerInfo = player.split(" ")[:5]
-        playerInfo[1] = "{0} {1}".format(
-            playerInfo[1], playerInfo[2]
-        )  # Combine first and last name into one element
-        playerInfo.pop(2)
-        playerInfo[-1] = playerInfo[-1].upper()  # Uppercase team abbreviations
         worksheet.write_row(row, 0, playerInfo)
         row += 1
 
 
 if __name__ == "__main__":
     workbook = createWorkbook()
-    getNHLPageSpreadsheet(workbook)
-    getESPNSpreadsheet(workbook)
+    getSpreadsheet(workbook, "NHL.com")
+    getSpreadsheet(workbook, "ESPN")
     workbook.close()
