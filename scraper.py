927988fd0705f4f3e8a20fcc32b687e2ec149371
alexrnguyen <alexander.nguyen@shaw.ca>
2023-08-18 13:41:52 -0600

    Create spreadsheet from NHL.com data

--- scraper.py
+++ scraper.py
@@ -1,10 +1,16 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import xlsxwriter
 
 punctuation = ".,"
 
 
-def getNHLPageSpreadsheet():
+def createWorkbook():
+    workbook = xlsxwriter.Workbook("nhl-fantasy-rankings-2023.xlsx")
+    return workbook
+
+
+def getNHLPageSpreadsheet(workbook):
     # Source: https://stackoverflow.com/questions/75771237/error-parsing-cert-retrieved-from-aia-as-der-error-couldnt-read-tbscertifi
     options = webdriver.ChromeOptions()
     options.add_experimental_option("excludeSwitches", ["enable-logging"])
@@ -18,16 +24,27 @@ def getNHLPageSpreadsheet():
     output = browser.find_element(By.XPATH, "//p[contains(text(), '1. ')]").text
     browser.quit()
 
+    worksheet = workbook.add_worksheet("NHL.com")
+
+    colNames = ["Rank", "Name", "Position", "Team"]
+    worksheet.write_row(0, 0, colNames)
     # Create an array from the output
     rankings = output.split("\n")
 
-    # Prepare player information for spreadsheet
+    # Insert player info into spreadsheet
+    row = 1
     for player in rankings:
         for char in player:
             if char in punctuation:
                 player = player.replace(char, "")
-        playerInfo = player.split(" ")[:4]
-        print(playerInfo)
+        playerInfo = player.split(" ")[:5]
+        playerInfo[1] = "{0} {1}".format(playerInfo[1], playerInfo[2])
+        playerInfo.pop(2)
+        worksheet.write_row(row, 0, playerInfo)
+        row += 1
 
 
-getNHLPageSpreadsheet()
+if __name__ == "__main__":
+    workbook = createWorkbook()
+    getNHLPageSpreadsheet(workbook)
+    workbook.close()
