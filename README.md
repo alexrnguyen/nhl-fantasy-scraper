69fb4bdbd0095906d5fb62e658c0f2db0cbecdde
alexrnguyen <alexander.nguyen@shaw.ca>
2023-08-18 18:05:59 -0600

    Resolve edge cases (ESPN spreadsheet)

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # nhl-fantasy-scraper
 
 A data scraper that extracts fantasy hockey rankings from NHL.com and ESPN into a spreadsheet. Built using Python, Selenium, and XlsxWriter.
+
+## Outstanding Issue
+
+- Players with multiple surnames (eg. Joel Eriksson Ek) have their position listed in the team column and their second surname listed in the position column
--- scraper.py
+++ scraper.py
@@ -2,8 +2,6 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import xlsxwriter
 
-punctuation = ".,"
-
 
 def createWorkbook():
     workbook = xlsxwriter.Workbook("nhl-fantasy-rankings-2023.xlsx")
@@ -43,14 +41,29 @@ def getSpreadsheet(workbook, source):
     # Insert player info into spreadsheet
     row = 1
     for player in rankings:
+        # Remove commas
         for char in player:
-            if char in punctuation:
+            if char == ",":
                 player = player.replace(char, "")
 
-        playerInfo = player.split(" ")[:5]
-        playerInfo[0] = int(playerInfo[0])
-        playerInfo[1] = "{0} {1}".format(playerInfo[1], playerInfo[2])
-        playerInfo.pop(2)
+        # Split into rank, first/last name, position, and team
+        if source == "NHL.com":
+            playerInfo = player.split(" ")[:5]
+        else:
+            playerInfo = player.split(" ")[:-1]
+        playerInfo[0] = int(playerInfo[0][:-1])  # Remove period after ranking
+
+        if len(playerInfo) == 5:
+            # Combine first and last name into 1 element
+            playerInfo[1] = "{0} {1}".format(playerInfo[1], playerInfo[2])
+            playerInfo.pop(2)
+        else:
+            # Edge Case (player has multiple first or last names -> eg. Trevor van Riemsdyk)
+            playerInfo[1] = "{0} {1} {2}".format(
+                playerInfo[1], playerInfo[2], playerInfo[3]
+            )
+            playerInfo.pop(2)
+            playerInfo.pop(2)
 
         if source == "ESPN":
             playerInfo[-1] = playerInfo[-1].upper()  # Uppercase team abbreviations
