2cd3c033881ea19bcf96ea908b3325a353247816
alexrnguyen <alexander.nguyen@shaw.ca>
2023-08-22 15:23:17 -0600

    Add average rankings to spreadsheet

--- scraper.py
+++ scraper.py
@@ -1,13 +1,16 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import xlsxwriter
+import xlrd
 
 
+# Create a new Excel spreadsheet
 def createWorkbook():
     workbook = xlsxwriter.Workbook("nhl-fantasy-rankings-2023.xlsx")
     return workbook
 
 
+# Create a sheet within an Excel spreadsheet containing the fantasy rankings from a given source
 def getSpreadsheet(workbook, source):
     # Source: https://stackoverflow.com/questions/75771237/error-parsing-cert-retrieved-from-aia-as-der-error-couldnt-read-tbscertifi
     options = webdriver.ChromeOptions()
@@ -58,9 +61,47 @@ def getSpreadsheet(workbook, source):
         worksheet.write_row(row, 0, playerInfo)
         row += 1
 
+    worksheet.autofilter("A1:D1")
+
+
+def getAverageRankings(workbook):
+    rankings = {}
+    readOnlyWorkbook = xlrd.open_workbook("nhl-fantasy-rankings-2023.xlsx")
+
+    numSheets = readOnlyWorkbook.nsheets
+    for i in range(numSheets):
+        worksheet = readOnlyWorkbook.sheet_by_index(i)
+        if readOnlyWorkbook.sheet_names()[i] == "Average Rankings":
+            continue
+        for row in range(1, worksheet.nrows):
+            if rankings.get(worksheet.cell_value(row, 1)) is None:
+                rankings[worksheet.cell_value(row, 1)] = [worksheet.cell_value(row, 0)]
+            else:
+                rankings[worksheet.cell_value(row, 1)].append(
+                    worksheet.cell_value(row, 0)
+                )
+
+    averageRankings = {}
+    for player in rankings.keys():
+        rankingArray = rankings.get(player)
+        averageRankings[player] = sum(rankingArray) / len(rankingArray)
+
+    avgRankingsWorksheet = workbook.add_worksheet("Average Rankings")
+    colNames = ["Name", "Average Rank"]
+    avgRankingsWorksheet.write_row(0, 0, colNames)
+
+    row = 1
+    for player in averageRankings.keys():
+        playerInfo = [player, averageRankings.get(player)]
+        avgRankingsWorksheet.write_row(row, 0, playerInfo)
+        row += 1
+
+    avgRankingsWorksheet.autofilter("A1:B1")
+
 
 if __name__ == "__main__":
     workbook = createWorkbook()
     getSpreadsheet(workbook, "NHL.com")
     getSpreadsheet(workbook, "ESPN")
+    getAverageRankings(workbook)
     workbook.close()
