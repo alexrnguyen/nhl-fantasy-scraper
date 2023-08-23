0fa2b48da6dbbdb8367aeeadb8ad072d81118c8a
alexrnguyen <alexander.nguyen@shaw.ca>
2023-08-23 02:03:54 -0600

    Add Yahoo rankings

--- scraper.py
+++ scraper.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import xlsxwriter
 import xlrd
 
@@ -22,6 +24,32 @@ def getSpreadsheet(workbook, source):
         browser.get(url)
         # Find the highest ranked player
         output = browser.find_element(By.XPATH, "//p[contains(text(), '1. ')]").text
+    elif source == "Yahoo":
+        url = "https://hockey.fantasysports.yahoo.com/hockey/draftanalysis"
+        output = ""
+        browser.get(url)
+
+        # Get Top 300 players (30 per page)
+        for i in range(10):
+            # Wait for table to load up
+            WebDriverWait(browser, 10).until(
+                EC.presence_of_element_located(
+                    (
+                        By.XPATH,
+                        "//div[@class='H(30px) W(100%) Bgc($shade) D(f) Ai(c) Jc(e)']",
+                    )
+                )
+            )
+            # Find all players on the current page
+            elements = browser.find_elements(By.XPATH, "//div[@data-tst='player-name']")
+            for element in elements:
+                output += element.text + "\n"
+
+            # Go to next page
+            buttons = browser.find_elements(By.XPATH, "//button[@role='presentation']")
+            nextPageButton = buttons[1]
+            nextPageButton.click()
+        output = output.strip()  # Remove new line character after last player
     else:
         url = "https://www.espn.com/fantasy/hockey/story/_/id/37963929/top-250-fantasy-nhl-rankings-scorers-goalies-2023-24"
         browser.get(url)
@@ -35,7 +63,10 @@ def getSpreadsheet(workbook, source):
     browser.quit()
 
     worksheet = workbook.add_worksheet(source)
-    colNames = ["Rank", "Name", "Position", "Team"]
+    if source == "Yahoo":
+        colNames = ["Rank", "Name"]
+    else:
+        colNames = ["Rank", "Name", "Position", "Team"]
     worksheet.write_row(0, 0, colNames)
 
     # Create an array from the output
@@ -44,24 +75,30 @@ def getSpreadsheet(workbook, source):
     # Insert player info into spreadsheet
     row = 1
     for player in rankings:
-        playerInfo = player.split(", ")
-        # Split first element into rank and player name components
-        playerInfo[0] = playerInfo[0].replace(".", "", 1)
-        playerInfo[0] = playerInfo[0].split(" ", 1)
-        playerInfo.insert(1, playerInfo[0][1])  # Add player name as its own element
-        playerInfo[0] = playerInfo[0][0]  # Ranking
-        playerInfo[0] = int(playerInfo[0])  # Remove period after ranking
+        # Player information on Yahoo's website is formatted quite differently. Handle Yahoo sheet in else clause
+        if source != "Yahoo":
+            playerInfo = player.split(", ")
+            # Split first element into rank and player name components
+            playerInfo[0] = playerInfo[0].replace(".", "", 1)
+            playerInfo[0] = playerInfo[0].split(" ", 1)
+            playerInfo.insert(1, playerInfo[0][1])  # Add player name as its own element
+            playerInfo[0] = playerInfo[0][0]  # Ranking
+            playerInfo[0] = int(playerInfo[0])  # Remove period after ranking
 
-        # Remove excess information from data (eg. health status, position ranking)
-        playerInfo[-1] = playerInfo[-1].split(" ")
-        playerInfo[-1] = playerInfo[-1][0]
-        if source == "ESPN":
-            playerInfo[-1] = playerInfo[-1].upper()  # Uppercase team abbreviations
+            # Remove excess information from data (eg. health status, position ranking)
+            playerInfo[-1] = playerInfo[-1].split(" ")
+            playerInfo[-1] = playerInfo[-1][0]
+            if source == "ESPN":
+                playerInfo[-1] = playerInfo[-1].upper()  # Uppercase team abbreviations
 
-        worksheet.write_row(row, 0, playerInfo)
+            worksheet.write_row(row, 0, playerInfo)
+            worksheet.autofilter("A1:D1")
+
+        else:
+            playerInfo = [row, player]
+            worksheet.write_row(row, 0, playerInfo)
+            worksheet.autofilter("A1:B1")
         row += 1
-
-    worksheet.autofilter("A1:D1")
 
 
 def getAverageRankings(workbook):
@@ -107,5 +144,6 @@ if __name__ == "__main__":
     workbook = createWorkbook()
     getSpreadsheet(workbook, "NHL.com")
     getSpreadsheet(workbook, "ESPN")
+    getSpreadsheet(workbook, "Yahoo")
     getAverageRankings(workbook)
     workbook.close()
